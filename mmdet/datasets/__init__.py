837703195cb05e48084b354a022091d5058a4b79
tuyen <tuyen@bytedance.com>
2022-03-10 19:50:22 +0800

    Add support for nuscenes dataset

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -8,6 +8,7 @@ from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
                                MultiImageMixDataset, RepeatDataset)
 from .deepfashion import DeepFashionDataset
 from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
+from .nuscenes import NuScenesDataset
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
 from .utils import (NumClassCheckHook, get_loading_pipeline,
                     replace_ImageToTensor)
@@ -18,7 +19,7 @@ from .xml_style import XMLDataset
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
     'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
-    'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
+    'LVISV1Dataset', 'NuScenesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
     'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
